0c6d2546e0a90851cbba9ac83cf147768a06196b
Michał Wróblewski <20270277+DeBos99@users.noreply.github.com>
2019-08-12 16:02:45 +0100

    Update .gitignore

--- main.pl
+++ main.pl
@@ -6,7 +6,7 @@ use File::Copy qw(copy);
 use File::Path qw(rmtree);
 use Term::ANSIColor qw(color colored);
 
-my $VERSION='1.0.9';
+my $VERSION='1.0.10';
 
 my $IP='0.0.0.0:80';
 my $name='Application';
